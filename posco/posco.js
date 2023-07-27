c2991d9747d06b6b94894b922fa9357d48e8e2f8
unknown <sumini94@naver.com>
2023-07-27 16:03:09 +0900

    포스코 반응형 수정

--- posco/posco.js
+++ posco/posco.js
@@ -2,39 +2,47 @@
 
 //스크롤시 헤더바 변경 230629 scrollY 또는 230630 scrollEx4 참고하여 해보기
 
-  $(window).on('scroll', () => {
-    // console.log(scrollY);
-    if (scrollY > 150) {
-      $('header').css({
-        width:'80%',
-        borderRadius:'25px',
-        margin:'0 auto',
-        transform:'translatey(2vw)',
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-        opacity: 1,
-      });
-    } else {
-      $('header').css({
-        width:'100%',   
-        opacity: 1,
-        borderRadius:'0',
-        transform:'translate(0)',
-        // marginTop:'1.5vw'
-    
-      });
-    }
-  });
-
-  // 햄버거버튼
-  $(document).ready(function () {
-    $('.ham_btn').on('click', (e) => {
-      e.preventDefault();
-      $('.ham_btn_list').slideToggle();
-      $('.ham_btn_list').toggleClass('hide');
+$(window).on('scroll', () => {
+  // console.log(scrollY);
+  if (scrollY > 150) {
+    $('header').css({
+      width: '80%',
+      borderRadius: '25px',
+      margin: '0 auto',
+      transform: 'translatey(2vw)',
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      opacity: 1,
     });
+  } else {
+    $('header').css({
+      width: '100%',
+      opacity: 1,
+      borderRadius: '0',
+      transform: 'translate(0)',
+      // marginTop:'1.5vw'
+    });
+  }
+});
+
+// 햄버거메뉴 열고 닫기
+// $(function () {
+//   $('.checkbox').on('click', () => {
+//     $('.checkmark span').toggleClass('active');
+//     $('.menu').slideToggle(); // == display: block
+//   });
+// });
+$(function () {
+  // 이 코드는 문서가 준비된 상태에서 실행됩니다. 즉, DOM이 완전히 로드된 상태에서 실행됩니다.
+
+  // 'checkbox' 클래스를 가진 요소를 클릭했을 때:
+  $('.checkbox').on('click', () => {
+    // 'active' 클래스를 가진 span 요소들의 상태를 토글합니다.
+    $('.checkmark span').toggleClass('active');
+
+    // 'menu' 클래스를 가진 요소를 슬라이드 토글합니다. (보이거나 숨깁니다.)
+    $('.menu').slideToggle(); // == display: block
   });
+});
 
-  // aos js
-  AOS.init();
-
-  
+// aos js
+AOS.init();
