933c0550320dae977d7960ca71eb417914249145
unknown <sumini94@naver.com>
2023-07-27 16:16:31 +0900

    포메인 업로드

---
+++ phomein/phomein.js
@@ -0,0 +1,22 @@
+//AOS
+AOS.init();
+
+
+function handleScroll() {
+    const header = document.querySelector('header');
+ 
+    if (window.scrollY > 150) {
+        header.style.height = '10vh';
+        header.style.lineHeight = '8vh';
+        header.style.margin = '0 auto';
+    } else {
+        header.style.height = '17vh';
+        header.style.lineHeight = 'initial';
+        header.style.margin = 'initial';
+    }
+}
+// // 스크롤 이벤트를 감지하여 핸들러 함수 호출
+window.addEventListener('scroll', handleScroll);
+
+
+
