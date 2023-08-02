4135ff5d533753729b7322e8ca3c6cbe124d83dd
unknown <sumini94@naver.com>
2023-08-02 10:13:00 +0900

    index 수정

--- index/index.js
+++ index/index.js
@@ -1,25 +1,16 @@
 // aos
 AOS.init();
 
-
 // 메인 portpolio svg 움직이기
-const wave_svg = document.querySelectorAll(div);
-$(window).on('scroll', () => {
-  wave_svg.forEach(function (div, idx) {
-    // console.log(length);
-    if (screenY > 10) {
-      div.style.strokeDasharray = length;
-      div.style.strokeDashoffset = 0;
-    } else {
-      svg.style.strokeDashoffset = length;
-    }
-  });
-});
-// 안돼 왜안돼...계속 안돼..그냥 안돼...
-
-// 버튼 스타일
-// $(".hover").mouseleave(
-//   function() {
-//     $(this).removeClass("hover");
-//   }
-// );
+// const wave_svg = document.querySelectorAll(div);
+// $(window).on('scroll', () => {
+//   wave_svg.forEach(function (div, idx) {
+//     // console.log(length);
+//     if (screenY > 10) {
+//       div.style.strokeDasharray = length;
+//       div.style.strokeDashoffset = 0;
+//     } else {
+//       svg.style.strokeDashoffset = length;
+//     }
+//   });
+// });
