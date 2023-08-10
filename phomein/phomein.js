916af79ddb3928680b0eb469791f7303ad77bdd0
sumiiiiiiin <sumini94@naver.com>
2023-08-10 21:12:05 +0900

    포메인수정

--- phomein/phomein.js
+++ phomein/phomein.js
@@ -1,6 +1,7 @@
-//AOS
+// AOS
 AOS.init();
 
+// 스크롤시 header의 nav_bar 이벤트 발생
 function handleScroll() {
   const header = document.querySelector('header');
 
@@ -14,8 +15,9 @@ function handleScroll() {
     header.style.margin = 'initial';
   }
 }
-// // 스크롤 이벤트를 감지하여 핸들러 함수 호출
+// 스크롤 이벤트를 감지하여 핸들러 함수 호출
 window.addEventListener('scroll', handleScroll);
+
 
 // 좌우 이미지 영역 스크롤 시 애니메이션 실행 코드
 // Intersection Observer를 사용하여 스크롤 이벤트 감지
@@ -26,8 +28,8 @@ const rightImg = document.querySelector('.right_img');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      leftImg.style.animation = 'move1 2.5s forwards';
-      rightImg.style.animation = 'move2 2.5s forwards';
+      leftImg.style.animation = 'move1 2.7s forwards';
+      rightImg.style.animation = 'move2 2.7s forwards';
     } else {
       leftImg.style.animation = 'none';
       rightImg.style.animation = 'none';
@@ -37,17 +39,10 @@ const observer = new IntersectionObserver((entries) => {
 
 observer.observe(sideIntro);
 
-//AOS실행될 때 gif어두워지기
-const videoSection = document.querySelector('.bg_video');
-
-document.addEventListener('aos:in', ({ detail }) => {
-  if (detail === videoSection) {
-    videoSection.classList.add('dark-bg');
-  }
-});
-
-document.addEventListener('aos:out', ({ detail }) => {
-  if (detail === videoSection) {
-    videoSection.classList.remove('dark-bg');
-  }
+// AOS실행될 때 gif어두워지기
+// 페이지 로딩 시 실행될 코드
+  window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementById('brightenImage').style.filter = 'brightness(0.4)';
+    }, 700); // 몇초 후에 이미지 밝기 조절
 });
